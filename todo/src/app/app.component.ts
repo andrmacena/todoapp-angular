b64ff9646c6d802c73d199ac160bd8ce2154da7a
André Macena <andrmacena_@outlook.com>
2021-11-18 22:06:40 -0300

    Adicionado método para remover item da lista

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -12,9 +12,24 @@ export class AppComponent {
 
   constructor() {
 
-    this.todos.push(new Todo('Ir ao supermercado', false, 1))
+    this.todos.push(new Todo('Ir ao supermercado', true, 1))
     this.todos.push(new Todo('Cortar o cabelo', false, 2))
     this.todos.push(new Todo('Passear com o cachorro', false, 3))
+
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {
+
+  }
+
+  markAsUndone() {
 
   }
 }
