1b1b128119587a537798a265cb1f619b0b42d373
André Macena <andrmacena_@outlook.com>
2021-11-23 21:07:23 -0300

    Criado métodos para salvar os itens no localstorage

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -13,17 +13,27 @@ export class AppComponent {
   public form: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.todos.push(new Todo('Ir ao supermercado', true, 1));
-    this.todos.push(new Todo('Cortar o cabelo', false, 2));
-    this.todos.push(new Todo('Passear com o cachorro', false, 3));
     this.form = this.fb.group({
-      title: ['',Validators.compose([
+      title: ['', Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(60),
         Validators.required
       ])]
     });
+    this.load();
+  }
 
+  add() {
+    const title = this.form.controls['title'].value
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(title, false, id));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
@@ -31,15 +41,28 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
-
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
 
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data !== null){
+      this.todos =  JSON.parse(data);
+    }
   }
 }
